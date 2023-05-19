de49308f0c61cabf9916835a485c54b6af011cd8
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-19 16:06:59 +0200

    Refactor pagina di dettaglio

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -6,20 +6,27 @@
             <li>
                 <a href="{{ route('admin.project.index')}}" class="btn btn-sm btn-danger">Back</a>
                 <h2 class="fs-4 text-secondary my-4">
-                    {{ $project->project_name }}
+                    {{ $project->nome }}
                 </h2>
             </li>
-            <li>{{ $project->description }}</li>
-            <li class="my-3">Creation Date : {{ $project->creation_date }}</li>
-            <li class="my-3">Price : {{ $project->price }} €</li>
-            <li class="my-3">Visibility : <span class="text-uppercase">{{ $project->visibility }}</span></li>
-            @if ($project->done == 1)
+            <li>{{ $project->descrizione }}</li>
+            <li class="my-3">Creation Date : {{ $project->data_di_creazione }}</li>
+            @if ($project->completato == 1)
                 <li class="d-flex align-items-center gap-2">  
-                    Done: <div class="circle done"></div>
+                    Completato: <div class="circle done"></div>
                 </li>
             @else
                 <li class="d-flex align-items-center gap-2">
-                    Done: <div class="circle work"></div>
+                    Completato: <div class="circle work"></div>
+                </li>
+            @endif
+            @if ($project->riscosso == 1)
+                <li class="d-flex align-items-center gap-2">  
+                     Riscosso: <div class="circle done"></div>
+                </li>
+            @else
+                <li class="d-flex align-items-center gap-2">
+                    Riscosso: <div class="circle work"></div>
                 </li>
             @endif
         </ul>
